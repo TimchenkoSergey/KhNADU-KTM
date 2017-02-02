7745dc8a4027134be9e903c99479099d10859200
Sergey Timchenko <art-street93@mail.ru>
2017-02-02 22:37:34 +0200

    Completed enrolle page

---
+++ js/enrollee.js
@@ -0,0 +1,21 @@
+(function() {
+	"use strict";
+
+    $(function(){
+        $(".partners__owl-carousel").owlCarousel({
+            items: 4,
+            loop: true,
+            dots: true,
+		    responsive: {
+		    	0   : { items: 1 },
+		        500 : { items: 2 },
+		        750 : { items: 3 },
+		        1100: { items: 4 }
+		    }
+        });
+    });
+
+    $(function($) {
+        $( ".swipebox" ).swipebox();
+    });
+})();
